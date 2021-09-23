19b86fd14ea87a687d34d1310033d467f9efb30d
João Victor <joao.lenny100@gmail.com>
2021-09-23 03:12:09 -0300

    6.2  quase la

--- fundamentos/bloco-6/README.md
+++ fundamentos/bloco-6/README.md
@@ -16,49 +16,23 @@ Quantas vezes você já se deparou com um formulário de cadastro, uma página d
 
 Após o curso da Trybe, no seu dia a dia de trabalho, será comum encontrar tarefas que, no fim das contas, são de criação de formulário. Por isso, é importante entender como um formulário funciona.
 
-## Introdução - CSS Flexbox
+## Aula 02: Bibliotecas JavaScript e Frameworks CSS
 
 ### O que vamos aprender?
 
-Você vai aprender sobre CSS Flexbox .
+Hoje você aprenderá como adicionar bibliotecas JavaScript e CSS aos seus sites.
 
-Desenvolver páginas web responsivas é uma tarefa muito importante, pois as pessoas acessam a Internet através de vários dispositivos com telas de diferentes tamanhos.
-
-Uma das abordagens para criar páginas responsivas é dividir a página em segmentos lógicos, usando cálculos baseados em porcentagem e, em seguida, ajustando os elementos para caberem nesse espaço definido.
-
-Infelizmente, a prática não é tão simples quanto descrito acima. Isso acontece porque os elementos não se assentam por padrão, próximos um do outro, como gostaríamos - pelo menos não os que têm a propriedade display configurada para ser block . Um outro problema comum é, depois de tudo configurado, a adição de margens e definição da propriedade float voltar a desconfigurar toda a estrutura inicialmente proposta.
-
-Como descrito acima, criar páginas responsivas utilizando float e posicionamento é um trabalho complicado.
-
-Para tornar esse trabalho mais fácil, você irá aprender a utilizar o CSS Flexbox , que é um módulo de layout que torna fácil o desenvolvimento de estruturas responsivas.
-
-Para isso, dividimos o conteúdo em duas partes:
-
-1 - Primeiro você aprenderá sobre as propriedades flex-direction , flex-wrap , flex-flow , reverse , justify-content , align-items e align-content . Todas essas propriedades serão aplicadas em containers .
-
-- CSS Flexbox - parte I
-
-2 - Agora, aprenderá sobre as propriedades align-self , order , flex-grow , flex-shrink e flex-basis . Também veremos como podemos trabalhar com margins no Flexbox . Essas propriedades serão aplicadas em items .
-
-- CSS Flexbox - parte II
-
-## Aula 03: CSS Flexbox - Parte 1
-
-### O que vamos aprender?
-
-Você vai aprender sobre CSS Flexbox Layout ( Flexible Box ). Flexbox é um módulo que visa fornecer uma maneira mais eficiente de organizar, alinhar e distribuir espaço entre os itens em um container , mesmo quando seu tamanho é desconhecido.
-
-Um container pode ser qualquer elemento HTML que contenha outros elementos, como, por exemplo, uma <div> ou <section> . Esse elemento pai é chamado de container , e os elementos filhos serão os itens.
-
-A ideia principal por trás do "layout flexível" é dar ao container a habilidade de alterar o tamanho ( width e height ) e a ordem dos itens para melhor ocupar o espaço disponível. Essa habilidade nos permite acomodar uma página em todos os tipos de dispositivos e tamanhos de tela.
+Existem literalmente milhares de bibliotecas JavaScript e CSS disponíveis gratuitamente na Internet, com os mais diversos propósitos. À medida que os sites que você desenvolve se tornam mais complexos, será preciso encontrar e integrar ferramentas que facilitem e acelerem o desenvolvimento.
 
 ### Você será capaz de:
 
-- Utilizar CSS Flexbox para criar layouts flexíveis;
-- Utilizar CSS Flexbox para alterar o comportamento de containers .
+- Adicionar e utilizar bibliotecas JavaScript e CSS em seus sites;
+- Utilizar bibliotecas JavaScript externas a partir de uma CDN .
 
 ### Por que isso é importante?
 
-O acesso à Internet não é feito apenas por computadores e notebooks. Tablets e principalmente smartphones também são utilizados para essa função. Com tantos dispositivos e tamanhos diferentes de telas, um design responsivo é necessário para promover a melhor experiência possível aos visitantes, independentemente do dispositivo e tela que estejam usando.
+No dia a dia do desenvolvimento de software, você vai precisar encontrar, selecionar e incorporar bibliotecas aos programas que você desenvolve. É muito importante conhecer os fundamentos de uma tecnologia, mas é importante também não reinventar a roda a todo momento. Por exemplo, imagine que você, toda vez que precisasse calcular a raiz quadrada de um número, tivesse que escrever uma função para isso, talvez utilizando uma fórmula matemática complexa! 😨Para que isso não seja necessário, outras pessoas já se deram o trabalho de escrever esse tipo de função de forma extremamente otimizada e disponibilizar para que outras pessoas utilizem.
 
-Flexbox é uma ferramenta que nos permitirá criar páginas com design responsivo de forma simples e organizada.
+O mesmo princípio se aplica ao desenvolvimento web. Existem muitos problemas que aparecem com frequência, à medida que a complexidade dos seus sites aumenta. Por exemplo, na aula anterior você aprendeu a construir formulários HTML . Imagine que, em um formulário de cadastro, você quer garantir que o nome e sobrenome sempre sejam preenchidos, que o email da pessoa tenha um formato válido e que seja impossível selecionar uma data de nascimento inexistente. Além disso, você acha que ficar digitando a data manualmente é cansativo, e quer colocar um campo que, ao ser clicado, abre um calendário para escolher a data. Como você faria isso? Com JavaScript (e um pouco de muito CSS , no caso do calendário). Isso, porém, é algo tão comum que já existem bibliotecas que fornecem essas funcionalidades prontas para você.
+
+Da mesma forma, existem muitos frameworks CSS que facilitam a construção de sites dinâmicos, acessíveis e responsivos, coisas com as quais você deve se preocupar sempre daqui para frente. Alguns exemplos mais famosos: Bulma , Bootstrap , Semantic UI e Materialize .
---
+++ fundamentos/bloco-6/aula-2/formularioDeCadastroEstilizado/script.js
@@ -0,0 +1,215 @@
+let informations = [];
+let activateChecksJs = false;
+
+const inputCpf = document.querySelector('#cpf');
+const inputCity = document.querySelector('#city');
+const inputDate = document.querySelector('#date');
+const inputName = document.querySelector('#name');
+const inputType = document.querySelector('#type');
+const inputEmail = document.querySelector('#email');
+const inputAddress = document.querySelector('#address');
+const inputOffice = document.querySelector('#office');
+const inputJobDescription = document.querySelector('#job-description');
+
+const sectionState = document.querySelector('#state');
+const textareaResumeSummary = document.querySelector('#resume-summary');
+
+const body = document.querySelector('body');
+const divInformationsPass = document.createElement('div');
+const divInformationsError = document.createElement('div');
+
+const buttonClear = document.querySelector('#clear');
+const buttonPreventValidations = document.querySelector('#prevent-validations');
+
+function generateOptions() {
+  const select = document.querySelector('select');
+  const estadosDoBrasil = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+
+  estadosDoBrasil.forEach((items) => {
+    const option = document.createElement('option');
+    select.appendChild(option).innerHTML = items;
+  });
+}
+
+function checkName() {
+  if (inputName.value.length <= 40 && inputName.value !== '') {
+    informations.push(inputName.value);
+  } else {
+    // alert('Valor de Nome Inválido!');
+    const p = document.createElement('p');
+    p.innerHTML = 'Valor de Nome Inválido!';
+    divInformationsError.appendChild(p);
+  }
+}
+
+function checkEmail() {
+  if (inputEmail.value.length <= 50 && inputEmail.value !== '') {
+    informations.push(inputEmail.value);
+  } else {
+    // alert('Valor de E-Mail Inválido!');
+    const p = document.createElement('p');
+    p.innerHTML = 'Valor de E-Mail Inválido!';
+    divInformationsError.appendChild(p);
+  }
+}
+
+function checkCpf() {
+  if (inputCpf.value.length <= 11 && !isNaN(inputCpf.value) && inputCpf.value !== '') {
+    informations.push(inputCpf.value);
+  } else {
+    // alert('Valor de CPF Inválido!');
+    const p = document.createElement('p');
+    p.innerHTML = 'Valor de CPF Inválido!';
+    divInformationsError.appendChild(p);
+  }
+}
+
+function checkAddress() {
+  if (inputAddress.value.length <= 200 && inputAddress.value !== '') {
+    informations.push(inputAddress.value);
+  } else {
+    // alert('Valor de Endereço Inválido!');
+    const p = document.createElement('p');
+    p.innerHTML = 'Valor de Endereço Inválido!';
+    divInformationsError.appendChild(p);
+  }
+}
+
+function checkCity() {
+  if (inputCity.value.length <= 28 && inputCity.value !== '') {
+    informations.push(inputCity.value);
+  } else {
+    // alert('Valor de Cidade Inválido!');
+    const p = document.createElement('p');
+    p.innerHTML = 'Valor de Cidade Inválido!';
+    divInformationsError.appendChild(p);
+  }
+}
+
+function checkState() {
+  informations.push(sectionState.value);
+}
+
+function checkType() {
+  if (inputType.value.length !== '') {
+    informations.push(inputCity.value);
+  } else {
+    // alert('Selecione uma opção(Casa ou Apartamento)!');
+    const p = document.createElement('p');
+    p.innerHTML = 'Selecione uma opção(Casa ou Apartamento)!';
+    divInformationsError.appendChild(p);
+  }
+}
+
+function checkResumeSummary() {
+  if (textareaResumeSummary.value.length <= 1000 && textareaResumeSummary.value !== '') {
+    informations.push(textareaResumeSummary.value);
+  } else {
+    // alert('Valor de Resumo do Currículo Inválido!');
+    const p = document.createElement('p');
+    p.innerHTML = 'Valor de Resumo do Currículo Inválido!';
+    divInformationsError.appendChild(p);
+  }
+}
+
+function checkOffice() {
+  if (inputOffice.value.length <= 40 && inputOffice.value !== '') {
+    informations.push(inputOffice.value);
+  } else {
+    // alert('Valor de Cargo Inválido!');
+    const p = document.createElement('p');
+    p.innerHTML = 'Valor de Cargo Inválido!';
+    divInformationsError.appendChild(p);
+  }
+}
+
+function checkJobDescription() {
+  if (inputJobDescription.value.length <= 500 && inputJobDescription.value !== '') {
+    informations.push(inputJobDescription.value);
+  } else {
+    // alert('Valor de Descrição do Serviço Inválido!');
+    const p = document.createElement('p');
+    p.innerHTML = 'Valor de Descrição do Serviço Inválido!';
+    divInformationsError.appendChild(p);
+  }
+}
+
+// function checkDate() {
+//   if (date.value.length === 10) {
+//     let year = '';
+//     for (let index = 0; index < date.value.length; index += 1) {
+//       if (index === 0 && date.value[index] <= 0 && date.value[index] > 31 || index === 1 && date.value[index] <= 0 && date.value[index] > 31
+//         || index === 2 && date.value[index] !== '/' || index === 5 && date.value[index] !== '/' || index === 3 && date.value[index] <= 0 &&
+//         date.value[index] > 12 || index === 4 && date.value[index] <= 0 && date.value[index] > 12 || index === 6 && date.value[index] < 0 ||
+//         index === 7 && date.value[index] < 0 || index === 8 && date.value[index] < 0 || index === 9 && date.value[index] < 0) {
+//         // alert('Valor de Data inválido. Verifique se contém as barras.');
+//         const p = document.createElement('p');
+//         p.innerHTML = 'Valor de Data inválido. Verifique se contém as barras.';
+//         divInformationsError.appendChild(p);
+//         break;
+//       } else {
+//         year += date.value[index];
+//       }
+//     }
+
+//     if (year.length === 10) {
+//       informations.push(year);
+//     }
+//   } else {
+//     // alert('Valor de Data inválido!');
+//     const p = document.createElement('p');
+//     p.innerHTML = 'Valor de Data inválido. Verifique se contém as barras.';
+//     divInformationsError.appendChild(p);
+//   }
+// }
+
+function checkAll() {
+  if (informations.length === 11) {
+    divInformationsError.remove();
+    informations.forEach((items) => {
+      const p = document.createElement('p');
+      p.innerHTML = items;
+      divInformationsPass.appendChild(p);
+    });
+    body.appendChild(divInformationsPass);
+  } else {
+    divInformationsPass.remove();
+    body.appendChild(divInformationsError);
+  }
+}
+
+generateOptions();
+
+buttonPreventValidations.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  checkName();
+  checkEmail();
+  checkCpf();
+  checkAddress();
+  checkCity();
+  checkState();
+  checkType();
+  checkResumeSummary();
+  checkOffice();
+  checkJobDescription();
+  // checkDate();
+  checkAll();
+
+  informations = [];
+});
+
+buttonClear.addEventListener('click', (() => {
+  inputCpf.value = '';
+  inputCity.value = '';
+  // inputDate.value = '';
+  inputName.value = '';
+  inputType.value = '';
+  inputEmail.value = '';
+  inputAddress.value = '';
+  inputOffice.value = '';
+  inputJobDescription.value = '';
+  sectionState.value = '';
+  textareaResumeSummary.value = '';
+  divInformationsError.remove();
+}));

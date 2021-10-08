fd7f34c1459f836d424ef30c824666f9e6161730
João Victor <joao.lenny100@gmail.com>
2021-10-08 00:53:29 -0300

    Atualizando

--- fundamentos/bloco-8/aula-2/exercicios/exercicio-7.js
+++ fundamentos/bloco-8/aula-2/exercicios/exercicio-7.js
@@ -69,7 +69,7 @@ const books = [
 const expectedResult = false;
 
 const authorUnique = () => {
-  return books.every((book) => !books.some((bookSome) =>
+  return books.every((book) => books.some((bookSome) =>
     (bookSome.author.birthYear === book.author.birthYear) &&
     (bookSome.author.name !== book.author.name)));
 };

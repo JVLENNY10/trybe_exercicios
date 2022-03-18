dcaefdc36bafeda575ea3e59789379e5475606ec
João Victor <joao.lenny100@gmail.com>
2022-03-17 22:42:53 -0300

    Parte dos exercicios do dia 22.2

--- 3-back-end/bloco-22/aula-2/exercicios.js
+++ 3-back-end/bloco-22/aula-2/exercicios.js
@@ -55,3 +55,82 @@ const callDoEquation = async () => {
 };
 
 callDoEquation();
+
+// 4) Realize o download deste arquivo e salve-o como simpsons.json. Utilize o arquivo baixado para realizar os requisitos abaixo.
+
+// - Você pode utilizar then e catch, async/await ou uma mistura dos dois para escrever seu código. Procure não utilizar callbacks.
+
+const fs = require('fs').promises;
+
+// - Crie uma função que leia todos os dados do arquivo e imprima cada personagem no formato id-Nome. Por exemplo: 1-Homer Simpson.
+
+const printPersonage = async () => {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf8');
+  const personages = JSON.parse(fileContent);
+  return personages.map((personage) => console.log(`${personage.id}-${personage.name}`));
+};
+
+printPersonage();
+
+// - Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja um personagem com o id informado, rejeite a Promise com o motivo "id não encontrado".
+
+const getPersonageId = async (id) => {
+  const personages = await fs.readFile('./simpsons.json', 'utf8')
+    .then((fileContent) => JSON.parse(fileContent));
+
+  const containId = personages.find((personage) => personage.id === id);
+  if (!containId) throw new Error('id não encontrado.');
+
+  return console.log(containId);
+};
+
+getPersonageId('10');
+
+// - Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.
+
+const changeFile = async () => {
+  const personages = await fs.readFile('./simpsons.json', 'utf8')
+    .then((fileContent) => JSON.parse(fileContent));
+
+  const newPersonagesList = personages.filer((personage) => personage.id !== 4 || personage.id !== 10);
+
+  const personages = await fs.writeFile('./simpsons1.json', newPersonagesList, { flag: 'w' })
+    .then(() => console.log('Salvo!'));
+};
+
+changeFile();
+
+// - Crie uma função que leia o arquivo simpsons.json e crie um novo arquivo, chamado simpsonFamily.json, contendo as personagens com id de 1 a 4.
+// - Crie uma função que adicione ao arquivo simpsonFamily.json o personagem Nelson Muntz.
+// - Crie uma função que substitua o personagem Nelson Muntz pela personagem Maggie Simpson no arquivo simpsonFamily.json.
+
+
+
+// const fs = require('fs').promises;
+
+// async function getSimpsonById() {
+//   const simpsons = await fs
+//     .readFile('./simpsons.json', 'utf-8')
+//     .then((fileContent) => JSON.parse(fileContent));
+
+//   const chosenSimpson = simpsons.find((simpson) => simpson.id === id);
+
+//   if (!chosenSimpson) {
+//     /* A palavra-chave `throw` dispara um erro que deve ser tratado por quem chamou nossa função.
+//      * Em funções `async`, utilizar `throw` faz com que a Promise seja rejeitada,
+//      * tendo como motivo o que passarmos para o `throw`.
+//      * Ou seja, a linha abaixo rejeita a Promise da nossa função com o motivo 'id não encontrado'
+//      */
+//     throw new Error('id não encontrado');
+//   }
+
+//   /* Da mesma forma que `throw` aciona o fluxo de erro e rejeita a Promise,
+//    * `return` aciona o fluxo de sucesso e resolve a Promise.
+//    * Sendo assim, a linha abaixo é equivalente a chamar `resolve(chosenSimpson)`
+//    * dentro do executor de uma Promise.
+//    */
+//   return chosenSimpson;
+// }
+
+
+// console.log(getSimpsonById());
